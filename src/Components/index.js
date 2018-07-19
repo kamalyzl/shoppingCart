13116da8a846369313c06163bd1b074a39e9efc5
kamalyzl <kamaly.kazal@gmail.com>
2018-07-19 03:55:31 -0500

    add readme

--- src/Components/index.js
+++ src/Components/index.js
@@ -19,17 +19,17 @@ class Home extends Component {
         return this.props.productsOrginial
       else return this.props.products
     }
-    
+
     return (
       <div className="container-fluid">
         <h1>Venta de Autos</h1>
         <SearchBar />
         <div className="row">
-          <div className="col-sm-12 col-md-8">
-            <Cards products={getData()} />
-          </div>
           <div className="col-sm-12 col-md-4">
             <List productsList={this.props.reducerList} />
+          </div>
+          <div className="col-sm-12 col-md-8">
+            <Cards products={getData()} />
           </div>
         </div>
       </div>
